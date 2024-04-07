0cb6673a9cc73d351807269e10fc56419f00b650
Ayman♛ <68021265+Ayman-YAN@users.noreply.github.com>
2024-04-07 16:39:39 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 (function () {
 
-var names = ["Ayman", "Jamal", "mohammed", "jassos", "anas", "moha", "lamia", "jasmine", "wassim", "johan"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
